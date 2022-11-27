e119af2bc9d4c2a5b9fdb6f2b7e8918c4aebcccf
ValeHenriquez <valentina.henriquez@alumnos.ucn.cl>
2022-11-27 18:55:59 -0400

    carga de datos a la base y script

--- database.sql
+++ database.sql
@@ -1,35 +1,37 @@
 drop table if exists customer cascade;
 drop table if exists "time" cascade;
+drop table if exists category cascade;
 drop table if exists product cascade;
 drop table if exists review cascade;
 
 create table customer(
-	
-	"index" int,
 	customer_id text primary key
 );
 
 create table "time"(
 	
-	"index" int,
 	"date" date primary key
 );
 
+create table category(
+	category_name text primary key
+);
+
 create table product(
-	
-	"index" int,
+
 	product_id text primary key,
 	product_parent text,
 	product_title text,
-	product_category text
+	product_category text references category(category_name)
 
 );
 
-create table review(
 
-	"index" int,
-	customer_id text references customer(customer_id),
+
+
+create table review(
 	marketplace text,
+	customer_id text references customer(customer_id),
 	review_id text primary key,
 	product_id text references product(product_id),
 	star_rating int,
@@ -42,5 +44,3 @@ create table review(
 	review_date date references "time"("date")
 );
 
-Gift_card
-gift card
