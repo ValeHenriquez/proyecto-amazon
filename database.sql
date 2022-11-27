bcd32889f1bd3fdd985563582311ee3f97c04dde
Daniel Zazzali <80864796+danielzazzali@users.noreply.github.com>
2022-11-27 16:40:28 -0300

    updated

--- database.sql
+++ database.sql
@@ -1,46 +1,43 @@
 drop table if exists customer cascade;
-drop table if exists marketplace cascade;
-drop table if exists category cascade;
+drop table if exists "time" cascade;
 drop table if exists product cascade;
 drop table if exists review cascade;
 
-
 create table customer(
-
+	
+	"index" int,
 	customer_id text primary key
 );
 
-create table marketplace(
-
-	marketplace text primary key
-);
-
-create table category(
-
-	category_name text primary key
+create table "time"(
+	
+	"index" int,
+	"date" date primary key
 );
 
 create table product(
-
+	
+	"index" int,
 	product_id text primary key,
 	product_parent text,
 	product_title text,
-	product_category text references Category(category_name) 
+	product_category text
+
 );
 
 create table review(
-	
-	marketplace text references Marketplace(marketplace)
-	customer text references Customer(customer_id),
+
+	"index" int,
+	customer_id text references customer(customer_id),
+	marketplace text,
 	review_id text primary key,
-	product text references Product(product_id),
-	star_rating int not null,
+	product_id text references product(product_id),
+	star_rating int,
 	helpful_votes int,
 	total_votes int,
-	vine bool,
-	verified_purchase bool,
+	vine text,
+	verified_purchase text,
 	review_headline text,
 	review_body text,
-	review_date date
-	
+	review_date date references "time"("date")
 );
